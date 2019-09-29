2dfee2af1262624ab148e8ee2e1f434c5a19105b
Abialbon Paul <dr.abialbon@gmail.com>
2019-09-29 17:33:53 +0100

    Sieve of Erathosthenes

---
+++ SieveOfErathosthenes/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int N = Integer.parseInt(args[0]);
+        boolean[] primeNos = new boolean[N+1];
+        // Initialising the array with true values
+        for(int i=2;i<=N;i++) {
+            primeNos[i] = true;
+        }
+        for(int i=2;i<=N/i;i++) {
+            if (primeNos[i] == false) continue;
+            if (isPrime(i)) {
+                for (int j = 2; j * i <= N; j++) {
+                    primeNos[j * i] = false;
+                }
+            }
+        }
+        int noOfPrimes = 0;
+        for (boolean b : primeNos) {
+            if (b) noOfPrimes++;
+        }
+        System.out.println(noOfPrimes);
+    }
+
+    public static boolean isPrime(int x) {
+        for (int i = 2; i<=x; i++) {
+            if ((x % i == 0) && (i != x)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
