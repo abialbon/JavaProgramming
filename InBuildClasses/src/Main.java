4ceec5befb55f1975cb7995cac8bea77b6504fa8
Abialbon Paul <dr.abialbon@gmail.com>
2019-09-29 11:00:55 +0100

    String formattting

--- InBuildClasses/src/Main.java
+++ InBuildClasses/src/Main.java
@@ -1,3 +1,7 @@
+import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
+
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,6 +18,22 @@ public class Main {
         // The == checks if the references are safe. They do not check the string literals themselves.
         System.out.println(firstName.equals(newName));
 
+        // Output is zero is equal, negative if the first string is lesser
+        // positive if the first string is greater
+        System.out.println(firstName.compareTo("Abe"));
 
+        // Case-insensitive comparisons
+        System.out.println(firstName.equalsIgnoreCase(name));
+        System.out.println(firstName.compareToIgnoreCase(name));
+
+        double a = 10, b = 6;
+        System.out.printf("%.3f", a/b);
+
+
+        double number = 1.6666666667;
+        DecimalFormat formatter = new DecimalFormat("##0.00");
+        JOptionPane.showMessageDialog(null, formatter.format(number));
+
+        System.exit(0);
     }
 }
